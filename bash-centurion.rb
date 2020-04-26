66133313739892ba5c6dedac47ca15f4cd308e40
chris.gillatt <chris.gillatt@autotrader.co.uk>
2020-04-26 12:01:49 +0100

    fix brew file

--- bash-centurion.rb
+++ bash-centurion.rb
@@ -1,4 +1,4 @@
-class Bash-centurion < Formula
+class BashCenturion < Formula
   desc "Centurion drinking game, written in bash"
   homepage "https://github.com/chris-gillatt/homebrew-bash-centurion"
   url "https://github.com/chris-gillatt/homebrew-bash-centurion/raw/master/tars/bash-centurion-0.0.1.tar.gz"
